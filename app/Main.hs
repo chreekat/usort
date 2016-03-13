f3b688d04a8498907b8b7f4566cdd470012f196e
Bryan Richter <b@chreekat.net>
2016-03-13 01:59:12 -0800

    Give it a CLI interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Prelude hiding (lines, getContents, putStrLn, unlines, concat, readFile)
 import Lib
-import System.IO
+import GHC.IO.Handle hiding (hGetContents)
+import System.IO hiding (hGetContents, putStrLn, readFile)
+import System.Environment
+import Data.Text
+import Data.Text.IO
 
 main :: IO ()
 main = do
+    as <- getArgs
+    items <- lines <$> case as of
+        [] -> hGetContents =<< hDuplicate stdin
+        xs -> concat <$> traverse readFile xs
     hSetBuffering stdout NoBuffering
     hSetBuffering stdin NoBuffering
-    print =<< uSort ["Best", "Bestest", "Worstest"]
+    (putStrLn . unlines) =<< uSort items
