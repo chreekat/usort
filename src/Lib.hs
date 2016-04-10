44f66dbcb44459284a43089c5b1a193a55b7998e
Bryan Richter <b@chreekat.net>
2016-04-09 22:39:14 -0700

    Refactor some code

--- src/Lib.hs
+++ src/Lib.hs
@@ -45,33 +45,30 @@ sortFunc xs = do
     half = length xs `div` 2
 
 goRight l h2 = do
-            er <- sortFunc h2
-            case er of
-                Left (SortEnded rs) -> pure (Left (SortEnded (l ++ rs)))
-                Left (Unsorted rs) -> do
-                    -- Left is sorted, right failed to sort.
-                    el' <- resort l
-                    case el' of
-                        Left (SortEnded ls') -> traceShow ls' $ error "l resort SortEnded"
-                        Left (Unsorted ls') -> pure (Left (Unsorted (ls' ++ rs)))
-                        Right l' -> goRight l' rs
-                Right r -> goMerge l r
+    er <- sortFunc h2
+    case er of
+        Left (SortEnded rs) -> pure (Left (SortEnded (l ++ rs)))
+        Left (Unsorted rs) -> redoLeft l rs
+        Right r -> goMerge l r
+
+redoLeft l rs = do
+    -- Left is sorted, right failed to sort.
+    el' <- resort l
+    case el' of
+        Left (SortEnded ls') -> traceShow ls' $ error "l resort SortEnded"
+        Left (Unsorted ls') -> pure (Left (Unsorted (ls' ++ rs)))
+        Right l' -> goRight l' rs
 
 goMerge l r = do
-                    em <- runExceptT (merge l r)
-                    case em of
-                        Left (MergeEnded xs) -> pure (Left (SortEnded xs))
-                        Left (Unmerged l' r') -> do
-                            er' <- resort r'
-                            case er' of
-                                Left (SortEnded rs') -> error "r' resort SortEnded"
-                                Left (Unsorted rs') -> do
-                                    el'' <- resort l'
-                                    case el'' of
-                                        Left (SortEnded ls'') -> error "l'' resort ended"
-                                        Left (Unsorted ls'') -> pure (Left (Unsorted (ls'' ++ rs')))
-                                        Right l'' -> goRight l'' rs'
-                                Right r'' -> goMerge l' r''
-                        Right sorted -> pure (Right sorted)
+    em <- runExceptT (merge l r)
+    case em of
+        Left (MergeEnded xs) -> pure (Left (SortEnded xs))
+        Left (Unmerged l' r') -> do
+            er' <- resort r'
+            case er' of
+                Left (SortEnded rs') -> error "r' resort SortEnded"
+                Left (Unsorted rs') -> redoLeft l' rs'
+                Right r'' -> goMerge l' r''
+        Right sorted -> pure (Right sorted)
 
 resort sorted = traceShow sorted $ error "resort"
