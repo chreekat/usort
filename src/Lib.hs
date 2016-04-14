a00a150b9788c6967944dcd37705c57cd01187fb
Bryan Richter <b@chreekat.net>
2016-04-13 22:17:52 -0700

    Implement resort
    The sameSize test is failing, amusingly.

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,6 +21,15 @@ data SortFail a = SortEnded [Sorted a]
                 | Unsorted [a]
     deriving (Show, Eq)
 
+-- sortFunc :: ( Show a
+--          , MonadReader (MVar (Maybe (MrgT a IO b))) mOuter
+--          , MonadIO mOuter)
+--       => [a]
+--       -> mOuter (Either (SortFail a) [Sorted a])
+sortFunc
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [a] -> f (Either (SortFail a) [Sorted a])
 sortFunc [] = pure (Right [])
 sortFunc [x] = pure (Right [S x B])
 sortFunc xs = do
@@ -34,14 +43,21 @@ sortFunc xs = do
     half = length xs `div` 2
     bases = map (\x -> S x B)
 
+redoLeft
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [Sorted a] -> [a] -> f (Either (SortFail a) [Sorted a])
 redoLeft l rs = do
-    -- Left is sorted, right failed to sort.
     el' <- resort l
     case el' of
         Left (SortEnded ls') -> traceShow ls' $ error "l resort SortEnded"
         Left (Unsorted ls') -> pure (Left (Unsorted (ls' ++ rs)))
         Right l' -> goRight l' rs
 
+goRight
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [Sorted a] -> [a] -> f (Either (SortFail a) [Sorted a])
 goRight l h2 = do
     er <- sortFunc h2
     case er of
@@ -49,6 +65,10 @@ goRight l h2 = do
         Left (Unsorted rs) -> redoLeft l rs
         Right r -> goMerge l r []
 
+redoRight
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [Sorted a] -> [Sorted a] -> f (Either (SortFail a) [Sorted a])
 redoRight l' r' = do
     er' <- resort r'
     case er' of
@@ -56,20 +76,27 @@ redoRight l' r' = do
         Left (Unsorted rs') -> redoLeft l' rs'
         Right r'' -> goMerge l' r'' []
 
-resort = undefined
--- resort = go [] []
---   where
---     go (l:ls) (r:rs) = goMerge (l:ls) (r:rs)
---     go left right = \case
---         [] -> pure (Right [])
---         [x] -> pure (Right [x])
---         (x:xs) ->
---             let (xs', [S last p]) = splitAt (length xs) (x:xs)
---             in case p of
---                 L p' -> go ((S last p') : left) right xs'
---                 R p' -> go left ((S last p') : right) xs'
---                 B -> error "wat"
+resort
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [Sorted a] -> f (Either (SortFail a) [Sorted a])
+resort = go [] []
+  where
+    go (l:ls) (r:rs) = goMerge (l:ls) (r:rs)
+    go left right = \case
+        [] -> pure (Right [])
+        [x] -> pure (Right [x])
+        (x:xs) ->
+            let (xs', [S last p]) = splitAt (length xs) (x:xs)
+            in case p of
+                L p' -> go ((S last p') : left) right xs'
+                R p' -> go left ((S last p') : right) xs'
+                B -> error "wat"
 
+goMerge
+  :: (Show a, MonadReader (MVar (Maybe (MrgT a IO b))) f,
+      MonadIO f) =>
+     [Sorted a] -> [Sorted a] -> [Sorted a] -> f (Either (SortFail a) [Sorted a])
 goMerge l r init = do
     em <- merge l r init
     case em of
