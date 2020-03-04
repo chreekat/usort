fda80a44d23d5c548f62e446e157a7ab358a8e9d
Bryan Richter <b@chreekat.net>
2020-03-04 23:01:55 +0200

    Unify implementation of usort and insort

--- app-usort/Main.hs
+++ app-usort/Main.hs
@@ -1,28 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude hiding (lines, getContents, putStr, unlines, concat, readFile)
-
-import GHC.IO.Handle hiding (hGetContents)
-import System.IO hiding (hGetContents, putStr, hPutStrLn, readFile)
-import System.Environment
-import Data.Text hiding (map)
-import Data.Text.IO
-import qualified Data.Text as T
-
-import USort
-import UserCompare
-import SplitItems
+import qualified App
+import UserCompare (userCompare)
 
 main :: IO ()
-main = do
-    as <- getArgs
-    (Items b is) <- splitItems . lines <$> case as of
-        [] -> do
-            dup <- hDuplicate stdin
-            maybe (pure ()) (hSetEncoding dup) =<< hGetEncoding stdin
-            hGetContents dup
-        xs -> concat <$> traverse readFile xs
-    hSetBuffering stdout NoBuffering
-    hSetBuffering stdin NoBuffering
-    (putStr . unlines . map (T.replicate b " " <>)) =<< usort userCompare (map (T.drop b) is)
+main = App.main userCompare
