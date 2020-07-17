ba16987bb9aa9ceb6c21465886bff824c9d1c0d7
Bryan Richter <bryan.richter@relexsolutions.com>
2020-07-17 18:34:52 +0300

    Add ghcide and bump to 20.03

--- default.nix
+++ default.nix
@@ -2,7 +2,22 @@
 , pkgs ? import sources.nixpkgs {}
 }:
 let
-  h = pkgs.haskell.lib;
-  gitignoreSrc =
-    (import sources."gitignore.nix" { inherit (pkgs) lib;}).gitignoreSource;
-in (pkgs.haskellPackages.callCabal2nix "usort" (gitignoreSrc (pkgs.lib.cleanSource ./usort)) {})
+    h = pkgs.haskell.lib;
+    hp = pkgs.haskellPackages;
+    gitignoreSrc =
+        (import sources."gitignore.nix" { inherit (pkgs) lib;}).gitignoreSource;
+    usort =
+        hp.callCabal2nix
+            "usort"
+            (gitignoreSrc (pkgs.lib.cleanSource ./usort)) {};
+in {
+    inherit usort;
+    shell = hp.shellFor {
+        packages = _ : [
+            usort
+        ];
+        buildInputs = [
+            hp.ghcide
+        ];
+    };
+}
