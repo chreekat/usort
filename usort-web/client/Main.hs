e9278d9ceb148f8dd89e202e45e771af3922bf50
Bryan Richter <b@chreekat.net>
2020-08-31 23:22:56 +0300

    Usort hello world

--- usort-web/client/Main.hs
+++ usort-web/client/Main.hs
@@ -1,33 +1,66 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Main where
 
 import Miso
 import Miso.String
 
-type Model = Int
+-- * Model shtuff
 
-data Action = Start | Bump
+data TopMode list res = Input | Process list | Result res
+deriving instance (Show list, Show res) => Show (TopMode list res)
+deriving instance (Eq list, Eq res) => Eq (TopMode list res)
+
+data Model list res = Model
+    { appMode :: TopMode list res
+    , input :: MisoString
+    }
+
+deriving instance (Show list, Show res) => Show (Model list res)
+deriving instance (Eq list, Eq res) => Eq (Model list res)
+
+-- * View shtuff
+
+inputView :: View Action
+inputView =
+    div_ []
+        [ h1_ [] [text "U Sort It"]
+        , textarea_
+            [ wrap_ "off", cols_ "80", rows_ "50"
+            , onInput UpdateInput
+            ]
+            [ text "Put yo shit here"]
+        , button_ [ onClick BeginSort ] [text "Click"]
+        ]
+
+sortingView ::  MisoString -> View Action
+sortingView list =
+    div_ []
+        [ div_ [] [text "You want me to sort this, yeah?"]
+        , pre_ [] [text list]
+        ]
+
+topView m = case appMode m of
+    Input -> inputView
+    Process list -> sortingView list
+
+
+data Action = Nope | UpdateInput MisoString | BeginSort
 
 main :: IO ()
 main = startApp App {..} where
-    initialAction = Start
-    model = 0
+    initialAction = Nope
+    model = Model Input "" :: Model MisoString ()
     update = update'
-    view = view'
+    view = topView
     events = defaultEvents
     subs = []
     mountPoint = Nothing
     logLevel = Off
 
 update' a m = case a of
-    Start -> noEff m
-    Bump -> noEff (m + 1)
-
-view' :: Model -> View Action
-view' m =
-    div_ []
-        [ button_ [ onClick Bump ] [ text "Bump" ]
-        , text (ms m)
-        ]
+    Nope -> noEff m
+    UpdateInput s -> noEff m { input = s }
+    BeginSort -> noEff m { appMode = Process (input m) }
