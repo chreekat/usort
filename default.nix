9f025149f2374ab3a27333ba73baa9466b4f6aa6
Bryan Richter <b@chreekat.net>
2020-09-06 07:39:00 +0300

    Successfully! build both apps

--- default.nix
+++ default.nix
@@ -7,29 +7,26 @@ let
     h = pkgs.haskell.lib;
     ghcPkgs = pkgs.haskellPackages;
     ghcjsPkgs = miso.pkgs.haskell.packages.ghcjs;
-    eitherPkgs =
-      if compiler == "ghc"
-        then ghcPkgs
-        else ghcjsPkgs;
     gitignoreSrc =
         (import sources."gitignore.nix" { inherit (pkgs) lib;}).gitignoreSource;
-in rec {
-    usort-lib = 
+    usort-lib = eitherPkgs:
         eitherPkgs.callCabal2nix
             "usort-lib"
             (gitignoreSrc (pkgs.lib.cleanSource ./usort-lib))
             {};
+in rec {
+
     usort-console =
         ghcPkgs.callCabal2nix
             "usort-console"
             (gitignoreSrc (pkgs.lib.cleanSource ./usort-console))
-            { inherit usort-lib; };
-    ## Usort for web
-    #usort-web-client =
-    #    ghcjsPkgs.callCabal2nix
-    #        "usort-web"
-    #        (pkgs.lib.cleanSource ./usort-web)
-    #        {};
+            { usort-lib = usort-lib ghcPkgs; };
+
+    usort-web-client =
+        ghcjsPkgs.callCabal2nix
+            "usort-web"
+            (pkgs.lib.cleanSource ./usort-web)
+            { usort-lib = usort-lib ghcjsPkgs; };
 
     shells = {
         ci = pkgs.mkShell {
