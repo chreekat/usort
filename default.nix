d93d02b48c5948fa63e0d08283f0c060276dc057
Bryan Richter <b@chreekat.net>
2020-09-20 17:01:40 +0300

    Switch to package set extension pattern

--- default.nix
+++ default.nix
@@ -1,40 +1,43 @@
 { sources ? import ./nix/sources.nix
-, compiler ? "ghc"
 }:
 let
     miso = import sources.miso {};
     pkgs = miso.pkgs;
     h = pkgs.haskell.lib;
-    ghcPkgs = pkgs.haskell.packages.ghc865;
-    ghcjsPkgs = pkgs.haskell.packages.ghcjs86;
+
     gitignoreSrc =
         (import sources."gitignore.nix" {}).gitignoreSource;
-    usort-lib = eitherPkgs:
-        eitherPkgs.callCabal2nix
-            "usort-lib"
-            (gitignoreSrc (pkgs.lib.cleanSource ./usort-lib))
-            {};
+
+    addUsortLib = self: super: {
+      usort-lib =
+        self.callCabal2nix
+          "usort-lib"
+          (gitignoreSrc (pkgs.lib.cleanSource ./usort-lib))
+          {};
+    };
+
+    ghcPkgs = pkgs.haskell.packages.ghc865.extend addUsortLib;
+    ghcjsPkgs = pkgs.haskell.packages.ghcjs86.extend addUsortLib;
+
 in rec {
 
     usort-console =
         ghcPkgs.callCabal2nix
             "usort-console"
             (gitignoreSrc (pkgs.lib.cleanSource ./usort-console))
-            { usort-lib = usort-lib ghcPkgs; };
+            {};
 
     usort-web-client =
         ghcjsPkgs.callCabal2nix
             "usort-web"
             (pkgs.lib.cleanSource ./usort-web)
-            { usort-lib = usort-lib ghcjsPkgs; };
+            {};
+
+    inherit (ghcPkgs) usort-lib;
 
     shells = {
         ci = pkgs.mkShell {
             buildInputs = [ pkgs.cachix ];
-        };
-        usort-lib = ghcPkgs.shellFor {
-          packages = p: [ (usort-lib p) ];
-          withHoogle = true;
         };
     };
 }
