deedca0460a8c6a0431a09ceaea0294e6a26ca87
Bryan Richter <b@chreekat.net>
2020-09-28 22:56:09 +0300

    Patch jsaddle

--- default.nix
+++ default.nix
@@ -10,49 +10,57 @@ let
 
     localSrc = dir: gitignoreSrc (pkgs.lib.cleanSource dir);
 
-    addUsortLib = self: super: {
+    extensions = self: super: {
       usort-lib =
         self.callCabal2nix
           "usort-lib"
           (localSrc ./usort-lib)
           {};
-    };
 
-    ghcPkgs = pkgs.haskell.packages.ghc865.extend addUsortLib;
-    ghcjsPkgs = pkgs.haskell.packages.ghcjs86.extend addUsortLib;
+      jsaddle-warp = super.jsaddle-warp.overrideAttrs (_:  {
+        #src = ../../src/jsaddle/jsaddle-warp;
+        patches = [ ./nix/patches/jsaddle.patch ];
+      });
+
+      usort-console =
+          self.callCabal2nix
+              "usort-console"
+              (localSrc ./usort-console)
+              {};
+
+      usort-web-client =
+          self.callCabal2nix
+              "usort-web"
+              (localSrc ./usort-web)
+              {};
+
+      usort-web-jsaddle =
+          self.callCabal2nix
+              "usort-web"
+              (localSrc ./usort-web)
+              { miso = miso.miso-jsaddle; };
+
+    };
+    reload-script = pkgs.writeScriptBin "reload" ''
+      ${ghcPkgs.ghcid}/bin/ghcid -c \
+        '${ghcPkgs.cabal-install}/bin/cabal new-repl' \
+        -o errors.err \
+        -r
+    '';
+
+    ghcPkgs = pkgs.haskell.packages.ghc865.extend extensions;
+    ghcjsPkgs = pkgs.haskell.packages.ghcjs86.extend extensions;
 
 in rec {
 
-    usort-console =
-        ghcPkgs.callCabal2nix
-            "usort-console"
-            (localSrc ./usort-console)
-            {};
+    inherit (ghcPkgs) usort-lib usort-console;
 
-    usort-web-client =
-        ghcjsPkgs.callCabal2nix
-            "usort-web"
-            (localSrc ./usort-web)
-            {};
-
-    inherit (ghcPkgs) usort-lib;
+    inherit (ghcjsPkgs) usort-web-client;
 
     shells = {
-        jsaddle =
-            let
-              a = ghcPkgs.callCabal2nix
-                "usort-web"
-                (localSrc ./usort-web)
-                { miso = miso.miso-jsaddle; };
-              reload-script = pkgs.writeScriptBin "reload" ''
-                ${ghcPkgs.ghcid}/bin/ghcid -c \
-                  '${ghcPkgs.cabal-install}/bin/cabal new-repl' \
-                  -o errors.err \
-                  -T ':main'
-              '';
-            in a.env.overrideAttrs (old: {
-              buildInputs = old.buildInputs ++ [reload-script];
-            });
+        jsaddle = ghcPkgs.usort-web-jsaddle.env.overrideAttrs (old: {
+          buildInputs = old.buildInputs ++ [reload-script];
+        });
 
         ci = pkgs.mkShell {
             buildInputs = [ pkgs.cachix ];
