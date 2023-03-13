a4fa2f6bbef9bb4f6cc43cf6c17baf0e8af6076c
Bryan Richter <b@chreekat.net>
2023-03-13 14:35:32 +0200

    Remove migrated app code

--- usort-lib/app-insort/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import qualified App
-import USort (realCompare)
-
-main :: IO ()
-main = App.main realCompare
--- usort-lib/app-usort/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import qualified App
-import UserCompare (userCompare)
-
-main :: IO ()
-main = App.main userCompare
